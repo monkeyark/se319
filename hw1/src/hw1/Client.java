019a0ee8792db52f9a964356eaa02d2a24e147b8
danryw <danryw@CO1313-15.ece.iastate.edu>
2018-09-09 18:07:03 -0500

    Work on Client and Server

--- hw1/src/hw1/Client.java
+++ hw1/src/hw1/Client.java
@@ -1,6 +1,79 @@
 package hw1;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Scanner;
+
 public class Client
 {
+	Socket serverSocket;
+	String serverHostname;
+	int serverPort;
+	ServerIO s_io;
+	
+	Client(String hostname, int port)
+	{
+		try
+		{
+			serverHostname = hostname;
+			serverPort = port;
+			
+			serverSocket = new Socket(hostname, port);
+		} catch (UnknownHostException e)
+		{
+			System.out.println("CLIENT SIDE: client excpetion");
+			e.printStackTrace();
+		} catch (IOException e)
+		{
+			e.printStackTrace();
+		}
+		
+		s_io = new ServerIO(this, serverSocket);
+		Thread t = new Thread(s_io);
+		t.start();
+		
+	}
+	
+	public void main(String[] args)
+	{
+		Client client = new Client("localHost", 5555);
+	}
+}
 
+class ServerIO implements Runnable
+{
+	Client c;
+	Socket s;
+	
+	ServerIO(Client client, Socket socket)
+	{
+		c = client;
+		s = socket;
+	}
+
+	@Override
+	public void run()
+	{
+		try
+		{
+			Scanner in;
+			PrintWriter out;
+			BufferedInputStream input = new BufferedInputStream(s.getInputStream());
+			BufferedOutputStream output = new BufferedOutputStream(s.getOutputStream());
+			in = new Scanner(input);
+			out = new PrintWriter(output);
+			
+			out.flush();
+			
+			//handle the inputStream and send the input to another client
+			
+		} catch (Exception e)
+		{
+			System.out.println("CLIENT SIDE: SERVERIO excpetion");
+		}
+	}
 }
--- hw1/src/hw1/Server.java
+++ hw1/src/hw1/Server.java
@@ -1,6 +1,85 @@
 package hw1;
 
+
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+import java.io.PrintWriter;
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.IOException;
+
 public class Server
 {
+	public static void main(String[] args) throws IOException
+	{
+		ServerSocket serverSocket = null;
+		int clientNum = 0;
+		
+		try
+		{
+			serverSocket = new ServerSocket(5555);
+		} catch (IOException e)
+		{
+			System.out.println("Could not listen on port: 5555");
+			System.exit(-1);
+		}	
+		
+		while(true)
+		{
+			Socket clientSocket = null;
+			
+			try
+			{
+				clientSocket = serverSocket.accept();
+				
+				ClientIO c = new ClientIO(clientSocket, clientNum++);
+				Thread t = new Thread(c);
+				t.start();
+				
+			} catch (Exception e)
+			{
+				System.out.println("SERVER SIDE: clientSocket excpetion");
+				System.exit(-1);
+			}
+		}
+	}
+}
 
+class ClientIO implements Runnable
+{
+	Socket s;
+	int num;
+	
+	ClientIO(Socket s, int num)
+	{
+		this.s = s;
+		this.num = num;
+	}
+	
+	@Override
+	public void run()
+	{
+		Scanner in;
+		PrintWriter out;
+		
+		try
+		{
+			BufferedInputStream input = new BufferedInputStream(s.getInputStream());
+			BufferedOutputStream output = new BufferedOutputStream(s.getOutputStream());
+			in = new Scanner(input);
+			out = new PrintWriter(output);
+			
+			out.println("You are client " + num);
+			out.flush();
+			
+			//handle the inputStream and send the input to another client
+			
+		} catch (IOException e)
+		{
+			System.out.println("SERVER SIDE: ClientIO Exception");
+		}
+		
+		
+	}
 }
