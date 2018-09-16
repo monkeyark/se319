795a0b11e9d51aa651165e627c5107a205d6a5b1
monkeyark <danryw@outlook.com>
2018-09-16 15:08:53 -0500

    add switch to next page

--- hw2/validation1.js
+++ hw2/validation1.js
@@ -1,49 +1,64 @@
 
 function validation()
 {
-    imgShow();
-    return false;  
+    if (imgShow())
+    {
+        //go next page
+    }
+    return false;
 }
 
-imgShow()
+function imgShow()
 {
+    var pass = false;
     first = document.getElementById("firstName").value;
     if(onlyNumLetter(first))
     {
         showCorrect("firstName");
+        pass = true;
     }
     else
     {
         showWrong("firstName");
+        pass = false;
     }
     last = document.getElementById("lastName").value;
     if(onlyNumLetter(last))
     {
         showCorrect("lastName");
+        pass = true;
     }
     else
     {
         showWrong("lastName");
+        pass = false;
     }
     gender = document.getElementById("gender").value;
     if(notEmpty(gender))
     {
         showCorrect("gender");
+        pass = true;
     }
     else
     {
         showWrong("gender");
+        pass = false;
     }
     state = document.getElementById("state").value;
     if(notEmpty(state))
     {
         showCorrect("state");
+        pass = true;
     }
     else
     {
         showWrong("state");
+        pass = false;
     }
+
+    return pass;
 }
+
 function onlyNumLetter(str)
 {
     return str.matches("^[A-z0-9]+$");
