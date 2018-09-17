5eb33b95c86a6d301961e604fe5832e0c71c406f
monkeyark <danryw@outlook.com>
2018-09-17 14:17:58 -0500

    bugfix in hw2

--- hw2/validation1.js
+++ hw2/validation1.js
@@ -15,50 +15,50 @@ function nextpage()
 
 function check()
 {
-    var pass = false;
+    var pass = true;
     var first = document.getElementById("firstName").value;
     if(alphanumeric(first))
     {
         showCorrect("firstNameimg");
-        pass = true;
+        pass = pass && true;
     }
     else
     {
         showWrong("firstNameimg");
-        pass = false;
+        pass = pass && false;
     }
     var last = document.getElementById("lastName").value;
     if(alphanumeric(last))
     {
         showCorrect("lastNameimg");
-        pass = true;
+        pass = pass && true;
     }
     else
     {
         showWrong("lastNameimg");
-        pass = false;
+        pass = pass && false;
     }
     var gender = document.getElementById("gender").value;
     if(notEmpty(gender))
     {
         showCorrect("genderimg");
-        pass = true;
+        pass = pass && true;
     }
     else
     {
         showWrong("genderimg");
-        pass = false;
+        pass = pass && false;
     }
     var state = document.getElementById("state").value;
     if(notEmpty(state))
     {
         showCorrect("stateimg");
-        pass = true;
+        pass = pass && true;
     }
     else
     {
         showWrong("stateimg");
-        pass = false;
+        pass = pass && false;
     }
 
     return pass;
--- hw2/validation2.js
+++ hw2/validation2.js
@@ -7,73 +7,71 @@ function validation2()
     return false;
 }
 
-
 function check()
 {
-    var pass = false;
+    var pass = true;
     var email = document.getElementById("email").value;
-    if (emailcheck(str))
+    if (emailcheck(email))
     {
-        showCorrect("email");
-        pass = true;
+        showCorrect("emailimg");
+        pass = pass && true;
     }
     else
     {
-        showWrong("email");
-        pass = false;
+        showWrong("emailimg");
+        pass = pass && false;
     }
     var tel = document.getElementById("tel").value;
     if (telcheck(tel))
     {
-        showCorrect("tel");
-        pass = true;
+        showCorrect("telimg");
+        pass = pass && true;
     }
     else
     {
-        showWrong("tel");
-        pass = false;
+        showWrong("telimg");
+        pass = pass && false;
     }
     var add = document.getElementById("add").value;
-    if (addresscheck(add))
+    if (addcheck(add))
     {
-        showCorrect("add");
-        pass = true;
+        showCorrect("addimg");
+        pass = pass && true;
     }
     else
     {
-        showWrong("add");
-        pass = false;
+        showWrong("addimg");
+        pass = pass && false;
     }
 
     return pass;
-
 }
 
 function emailcheck(str)
 {
-    return str.matches("^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$");
+    return /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$/.test(str);
 }
 
 function telcheck(str)
 {
-    return str.matches("^[0-9]{3}-[0-9]{3}-[0-9]{4}|\d{10}$");
+    return /^[0-9]{3}-[0-9]{3}-[0-9]{4}|\d{10}$/.test(str);
 }
 
 function addcheck(str)
 {
-    var pass = false;
+    if (!/^[A-Za-z,]+$/.test(str)) return false;
+    if (str[0] == "," || str[str.length-1] == ",") return false;
     var i;
-    for (i=0;i<str.length; i++)
+    var count = 0;
+    for (i=0; i<str.length; i++)
     {
-        if (str.charAt(i) == ',')
+        if (str[i] == ",")
         {
-            continue;
-        }
-        if (str[i].matches("^[A-za-z]$"))
-        {
-            pass = /^[a-z]+$/i.test(str);
+            count++;
         }
     }
+
+    return count == 1;
 }
 
 function showCorrect(id)
